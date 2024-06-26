4a09af01edfb96e34c74ce1d033b177e602e6207
Yauheni Dubovik <yauheni_dubovik@epam.com>
2024-06-26 14:53:03 +0400

    feat(TASK-5): s3 integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,8 @@ const API_PATHS = {
   product:
     "https://luwm75ht9a.execute-api.eu-central-1.amazonaws.com/prod/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import:
+    "https://xt3lx7qwli.execute-api.eu-central-1.amazonaws.com/prod/import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
